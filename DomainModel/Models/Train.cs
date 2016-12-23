28085a3182fbd3c93c4c2a1aa45557af5cb7e313
Magdalena Panasiuk <magdalena.panasiukk@gmail.com>
2016-12-23 14:01:30 +0100

    co

--- DomainModel/Models/Train.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DomainModel.Models
-{
-    public class Train:Entity
-    {
-        public virtual string Name { get; set; }
-        public virtual int SeatNo { get; set; }
-        public bool IsArchival { get; set; }
-    }
-}
---
+++ GUI/FormToDetails.cs
@@ -0,0 +1,50 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FormToDetails : Form
+    {
+        public Form1 oldform;
+        public FormToDetails(Form1 f)
+        {
+            oldform = f;
+            InitializeComponent();
+            DetailsView();
+        }
+        public void DetailsView()
+        {
+            var listofticekets = oldform.connectiontodetails;
+            if (listofticekets == null)
+            {
+                MessageBox.Show("Nie ma połączenia do wyświetlenia szczegółów");
+                return;
+            }
+            dataGridView.Rows.Clear();
+            var x = new DataGridViewRow();
+            int i = 0;
+            foreach (var elem in listofticekets)
+            {
+                x.Tag = elem;
+
+                dataGridView.Rows.Add(x);
+                dataGridView.Rows[i].Cells[0].Value = elem.ConnectionDefinition.Departure.Name;
+                dataGridView.Rows[i].Cells[1].Value = elem.ConnectionDefinition.Arrival.Name;
+                dataGridView.Rows[i].Cells[2].Value = elem.DepartureTime;
+                dataGridView.Rows[i].Cells[3].Value = elem.ArrivalTime;
+                dataGridView.Rows[i].Cells[4].Value = elem.ConnectionDefinition.Price;
+                i++;
+            }
+        }
+
+        
+    }
+}
