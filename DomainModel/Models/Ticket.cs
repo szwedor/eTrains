66e0f52e186dc21899a4552787e635ac7a4e0f11
Magdalena Panasiuk <magdalena.panasiukk@gmail.com>
2016-12-23 14:08:39 +0100

    e

---
+++ DomainModel/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DomainModel.Models
+{
+    public class Ticket:Entity
+    {
+        public enum Discount {Without,Student };
+        public virtual User User { get; set; }
+        public virtual Connection Connection { get; set; }
+        public int Seat { get; set; }
+    }
+}
